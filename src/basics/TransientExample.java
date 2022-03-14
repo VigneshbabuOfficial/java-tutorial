a6ced66f247febc85b1983a13e9e9f6c64d4c813
vigneshofficial2020 <vigneshofficial2020@gmail.com>
2022-03-14 23:49:29 +0530

    transient keyword example

---
+++ src/basics/TransientExample.java
@@ -0,0 +1,74 @@
+package basics;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class TransientExample implements Serializable {
+
+	public int nonTrans = 0;
+
+	public String nonTransStr = "nonTransStr";
+
+	public transient int transInt = 0;
+
+	private static void serialize() {
+
+		TransientExample tr = new TransientExample();
+
+		tr.nonTrans = 100;
+		tr.nonTransStr = "edison";
+		tr.transInt = 99;
+
+		try {
+
+			FileOutputStream fileOut = new FileOutputStream("D:\\testing\\transient-demo.txt");
+
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(tr);
+			out.close();
+			fileOut.close();
+			System.out.println("serialized data is stored in " + "D:\\testing\\transient-demo.txt");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void deserialize() {
+
+		TransientExample objFromFile = null;
+
+		try {
+
+			FileInputStream fileIn = new FileInputStream("D:\\testing\\transient-demo.txt");
+
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+
+			objFromFile = (TransientExample) in.readObject();
+
+			in.close();
+			fileIn.close();
+
+			System.out.println("Deserialization completed :");
+			System.out.println(" objFromFile.nonTransStr = " + objFromFile.nonTransStr);
+			System.out.println(" objFromFile.nonTrans = " + objFromFile.nonTrans);
+			System.out.println(" objFromFile.transInt = " + objFromFile.transInt);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	public static void main(String[] args) {
+		serialize();
+		
+		deserialize();
+	}
+
+}
