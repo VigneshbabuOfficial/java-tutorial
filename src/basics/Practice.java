87d28acebf432a5bbe688dd01f8ea80c5ed3951c
vigneshofficial2020 <vigneshofficial2020@gmail.com>
2022-03-14 22:55:16 +0530

    final keyword example

--- src/basics/Practice.java
+++ src/basics/Practice.java
@@ -2,9 +2,62 @@ package basics;
 
 public class Practice {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	final int a;
+	final static int b;
+	final int c=10;
+	final static int d=100;
 
+	{
+		a = 25;
+		
+//		ERROR : The final field Practice.b cannot be assigned -> static TO static concept
+//		b = 25;
+		
+//		ERROR : The final field Practice.c cannot be assigned
+//		c = 25;
+		
+//		ERROR : The final field Practice.d cannot be assigned
+//		d = 25;
+	}
+
+	static {
+		
+//		ERROR : Cannot make a static reference to the non-static field a
+//		a = 25;
+		
+		b = 25;
+		
+//		ERROR : Cannot make a static reference to the non-static field c
+//		c = 25;
+		
+//		ERROR : The final field Practice.d cannot be assigned
+//		d = 25;
+	}
+
+	private void assign() {
+		
+//		ERROR : The final field Practice.a b c d cannot be assigned
+//		a = 25;
+//		b = 25;
+//		c = 25;
+//		d = 25;
+		
+	}
+
+	public static void main(String[] args) {
+		
+//		ERROR : Cannot make a static reference to the non-static field a
+//		a = 25;
+		
+//		ERROR : The final field Practice.b cannot be assigned
+//		b = 25;
+		
+//		ERROR : Cannot make a static reference to the non-static field c
+//		c = 15;
+		
+//		ERROR : The final field Practice.d cannot be assigned
+//		d = 15;
+		
 	}
 
 }
